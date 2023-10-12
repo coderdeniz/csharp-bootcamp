70ccd75debb8eb0e7576ac41ef9ff8c2af90b4e9
coderdeniz <denizdumanresmi@gmail.com>
2023-10-13 00:52:15 +0300

    2 / day-8

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,11 +1,12 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
-ProductManager productManager = new ProductManager(new InMemoryProductDal());
+ProductManager productManager = new ProductManager(new EfProductDal());
 
-foreach (var p in productManager.GetAll())
+foreach (var p in productManager.GetAllByUnitPrice(3,100).OrderByDescending(p=>p.UnitPrice))
 {
-    Console.WriteLine(p.ProductName);
+    Console.WriteLine(p.ProductName + " - price: " + p.UnitPrice + "$");
 }
 
 
