bfe0238f0a095f59b51a4f2c03a46490e488f263
coderdeniz <denizdumanresmi@gmail.com>
2023-10-17 23:14:40 +0300

    1 / day-10

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -20,6 +21,12 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            _productDal.Add(product);
+            return new Result(true,"Ürün eklendi");
+        }
+
         public List<Product> GetAll()
         {
             // iş kodları yazılır
@@ -36,6 +43,11 @@ namespace Business.Concrete
         public List<Product> GetAllByUnitPrice(decimal min, decimal max)
         {
             return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p=>p.ProductId == productId);    
         }
 
         public List<ProductDetailDto> GetProductDetails()
